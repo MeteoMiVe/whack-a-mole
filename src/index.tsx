1f3a6e540feb1dddb0908131b0f70653e82e7bc9
Michael Verhaeghe <michael_verhaeghe@hotmail.com>
2023-10-07 10:31:23 +0200

    test husky

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import logo from './logo.svg'
 
 console.log('gimme a warning')
+console.log('gimme a warning')
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
