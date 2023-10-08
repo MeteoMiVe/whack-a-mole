820d4da0bfd3ba51d8fb973ee0c6b512e2f93e4b
Michael Verhaeghe <michael_verhaeghe@hotmail.com>
2023-10-08 17:42:09 +0200

    Solved caching issue, new express config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,13 @@
-import express, { Request, Response } from 'express';
-
 import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express Server!');
+app.get('/api/scores', (req: Request, res: Response) => {
+  res.send('hello scores 1234');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
