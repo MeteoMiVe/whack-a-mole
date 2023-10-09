805133c8d784b5f6a91c3e5dfe17d2fa94e6a390
Michael Verhaeghe <michael_verhaeghe@hotmail.com>
2023-10-09 10:41:17 +0200

    added comonent switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css';
 
 import Game from './components/Game/Game';
-//import LeaderBoard from './components/LeaderBoard/LeaderBoard';
+import LeaderBoard from './components/LeaderBoard/LeaderBoard';
+import { GameState } from './store/features/game/gameSlice';
+import { useAppSelector } from './store/store';
 
 const App = () => {
-  return (
-    <div className="App">
-      {/*  <LeaderBoard /> */}
-      <Game />
-    </div>
-  );
+  const gameState = useAppSelector((state) => state.game.gameState);
+
+  const componentMap: Record<GameState, JSX.Element | null> = {
+    waiting: null,
+    running: <Game />,
+    ended: <LeaderBoard />
+  };
+
+  return <div className="App">{componentMap[gameState]}</div>;
 };
 
 export default App;
